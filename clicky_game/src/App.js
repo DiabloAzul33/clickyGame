89ba5676053df7cd87e3303bcbd6cf9e6857d58a
DiabloAzul <curr33@gmail.com>
2019-09-07 10:30:01 -0600

    shuffle and score fxning

--- clicky_game/src/App.js
+++ clicky_game/src/App.js
@@ -7,7 +7,8 @@ import data from './data';
 class App extends React.Component {
   state = {
     cocktails: data,
-    beenClicked: []
+    beenClicked: [],
+    score: 0
   }
 
   displayCocktails = () => {
@@ -18,12 +19,40 @@ class App extends React.Component {
     console.log("Cocktail has been clicked.");
     var cocktail = event.currentTarget.getAttribute("value");
     console.log(cocktail);
+    if (this.state.beenClicked.includes(cocktail)) {
+      console.log(`You lose! ${cocktail} has already been clicked`);
+      alert(`You lose! ${cocktail} has already been clicked`);
+      this.setState({
+        beenClicked: [],
+        score: 0
+      });
+    } else {
+      this.setState({
+        beenClicked: this.state.beenClicked.concat(cocktail),
+        score: this.state.score + 1
+      }, () => {
+        if(this.state.cocktails.length === this.state.beenClicked.length) {
+        alert("You win and you're drunk! Don't forget to tip your bartender and Uber home");
+        this.setState({
+          beenClicked: [],
+          score: 0
+        });
+      }
+    }
+    );
+    }
+    this.shakeCocktails();
   }
 
-  render(){
+shakeCocktails = () => {
+  this.state.cocktails.sort(() => .5 - Math.random());
+}
+
+render(){
   return (
     <div className="App">
       <h1>Cocktail Clicky Game!</h1>
+      <h3>Score: {this.state.score}</h3>
       {this.displayCocktails()}
     </div>
   );
--- clicky_game/src/data.js
+++ clicky_game/src/data.js
@@ -3,10 +3,10 @@ var data = [
         name: "White Russian",
         url: "https://static.vinepair.com/wp-content/uploads/2017/11/white-russian-inside.jpg"
     },
-    {
-        name: "Old Fashioned",
-        url:"https://static.vinepair.com/wp-content/uploads/2017/11/old-fashioned-inside.jpgttps://static.vinepair.com/wp-content/uploads/2019/01/2019cocktails_internal_rumoldfashioned.jpg"
-    },
+    // {
+    //     name: "Old Fashioned",
+    //     url:"https://static.vinepair.com/wp-content/uploads/2017/11/old-fashioned-inside.jpgttps://static.vinepair.com/wp-content/uploads/2019/01/2019cocktails_internal_rumoldfashioned.jpg"
+    // },
     {
         name: "Painkiller",
         url: "https://static.vinepair.com/wp-content/uploads/2019/01/2019cocktails_internal_painkiller.jpg"
@@ -37,7 +37,7 @@ var data = [
     },
     {
         name: "Paloma",
-        url: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjcxs-1u7DkAhXeGTQIHTEkDYwQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.bonappetit.com%2Frecipe%2Fpaloma&psig=AOvVaw3tLf4HEJdxDlTsMd5TJpgu&ust=1567456172520233"
+        url: "https://assets.bonappetit.com/photos/57ae3888f1c801a1038bd14b/1:1/w_768,h_768,c_limit/paloma.jpg"
     }
 ]
 
